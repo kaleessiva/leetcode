0b91f95dbf4074091e6464e7b34c24b9dd706896
Siva S <133769219+kaleessiva@users.noreply.github.com>
2025-04-21 11:26:06 +0530

    Time: 0 ms (100%), Space: 40.5 MB (58.02%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximum69Number (int num) {
+        char ar[] = String.valueOf(num).toCharArray();
+       	for(int i =0;i<ar.length;i++){   
+        if(ar[i]=='6'){                        
+			ar[i]='9';
+            break;
+        }
+        
+    }
+	return Integer.parseInt(new String(ar));
+    }
+}
